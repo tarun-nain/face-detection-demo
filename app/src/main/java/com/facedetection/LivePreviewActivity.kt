cb1128383376fbf88210f9f64e4aa326623868d4
mobcoder-tarun-kumar <tarun.kumar@mobcoder.com>
2020-07-03 01:34:10 +0530

    cheeks center distance

---
+++ app/src/main/java/com/facedetection/LivePreviewActivity.kt
@@ -0,0 +1,218 @@
+package com.facedetection
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.facedetection.facedetector.FaceDetectorProcessor
+import com.google.android.gms.common.annotation.KeepName
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.IOException
+import java.util.*
+
+/** Live preview demo for ML Kit APIs.  */
+@KeepName
+class LivePreviewActivity : AppCompatActivity(),
+    ActivityCompat.OnRequestPermissionsResultCallback,
+    CompoundButton.OnCheckedChangeListener {
+
+    private var cameraSource: CameraSource? = null
+    private var preview: CameraSourcePreview? = null
+    private var graphicOverlay: GraphicOverlay? = null
+    private var selectedModel = FACE_DETECTION
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+        setContentView(R.layout.activity_live_preview)
+
+        preview = findViewById(R.id.preview)
+        if (preview == null) {
+            Log.d(TAG, "Preview is null")
+        }
+
+        graphicOverlay = findViewById(R.id.graphic_overlay)
+        if (graphicOverlay == null) {
+            Log.d(TAG, "graphicOverlay is null")
+        }
+
+
+        if (allPermissionsGranted()) {
+            createCameraSource(selectedModel)
+        } else {
+            runtimePermissions
+        }
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        Log.d(TAG, "Set facing")
+        if (cameraSource != null) {
+            if (isChecked) {
+                cameraSource?.setFacing(CameraSource.CAMERA_FACING_FRONT)
+            } else {
+                cameraSource?.setFacing(CameraSource.CAMERA_FACING_BACK)
+            }
+        }
+        preview?.stop()
+        startCameraSource()
+    }
+
+    private fun createCameraSource(model: String) {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = CameraSource(this, graphicOverlay)
+        }
+        try {
+            when (model) {
+                FACE_DETECTION -> {
+                    Log.i(TAG, "Using Face Detector Processor")
+
+                    val faceDetectorOptions = FaceDetectorOptions.Builder()
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setMinFaceSize(0.1f)
+                        .enableTracking()
+                        .build()
+
+                    cameraSource!!.setMachineLearningFrameProcessor(
+                        FaceDetectorProcessor(this, faceDetectorOptions)
+                    )
+                }
+
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Can not create image processor: $model", e)
+            Toast.makeText(
+                applicationContext, "Can not create image processor: " + e.message,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private fun startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null")
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null")
+                }
+                preview!!.start(cameraSource, graphicOverlay)
+            } catch (e: IOException) {
+                Log.e(TAG, "Unable to start camera source.", e)
+                cameraSource!!.release()
+                cameraSource = null
+            }
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+        createCameraSource(selectedModel)
+        startCameraSource()
+    }
+
+    /** Stops the camera.  */
+    override fun onPause() {
+        super.onPause()
+        preview?.stop()
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        if (cameraSource != null) {
+            cameraSource?.release()
+        }
+    }
+
+    private val requiredPermissions: Array<String?>
+        get() = try {
+            val info = this.packageManager
+                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            if (ps != null && ps.size > 0) {
+                ps
+            } else {
+                arrayOfNulls(0)
+            }
+        } catch (e: Exception) {
+            arrayOfNulls(0)
+        }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in requiredPermissions) {
+            if (!isPermissionGranted(this, permission)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private val runtimePermissions: Unit
+        get() {
+            val allNeededPermissions: MutableList<String?> = ArrayList()
+            for (permission in requiredPermissions) {
+                if (!isPermissionGranted(this, permission)) {
+                    allNeededPermissions.add(permission)
+                }
+            }
+            if (!allNeededPermissions.isEmpty()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    allNeededPermissions.toTypedArray(),
+                    PERMISSION_REQUESTS
+                )
+            }
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "Permission granted!")
+        if (allPermissionsGranted()) {
+            createCameraSource(selectedModel)
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val OBJECT_DETECTION = "Object Detection"
+        private const val OBJECT_DETECTION_CUSTOM = "Custom Object Detection (Birds)"
+        private const val FACE_DETECTION = "Face Detection"
+        private const val TEXT_RECOGNITION = "Text Recognition"
+        private const val BARCODE_SCANNING = "Barcode Scanning"
+        private const val IMAGE_LABELING = "Image Labeling"
+        private const val IMAGE_LABELING_CUSTOM = "Custom Image Labeling (Birds)"
+        private const val AUTOML_LABELING = "AutoML Image Labeling"
+        private const val TAG = "LivePreviewActivity"
+        private const val PERMISSION_REQUESTS = 1
+        private fun isPermissionGranted(
+            context: Context,
+            permission: String?
+        ): Boolean {
+            if (ContextCompat.checkSelfPermission(context, permission!!)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.i(TAG, "Permission granted: $permission")
+                return true
+            }
+            Log.i(TAG, "Permission NOT granted: $permission")
+            return false
+        }
+    }
+}
